ec5f6fbf31c7add5baa471a255c72ced9a1cd4b8
Nguyen Chi Thanh <thanhnguyenchice@gmail.com>
2021-03-23 15:51:08 +0700

    Initial Commit

---
+++ WonderfulCinema/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WonderfulCinema
+//
+//  Created by Nguyen Chi Thanh on 23/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WonderfulCinema/WonderfulCinemaApp.swift
@@ -0,0 +1,17 @@
+//
+//  WonderfulCinemaApp.swift
+//  WonderfulCinema
+//
+//  Created by Nguyen Chi Thanh on 23/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct WonderfulCinemaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
